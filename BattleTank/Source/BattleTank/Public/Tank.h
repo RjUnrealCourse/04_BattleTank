25c486a77af1622ba7982565a57319b977bc4ebe
Ramachandra Junior <ajaykumarch33@yahoo.com>
2017-12-25 01:54:53 +0530

    Tank class full clean up

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -13,14 +13,6 @@ class BATTLETANK_API ATank : public APawn
 {
 	GENERATED_BODY()
 
-private:
-    UPROPERTY(EditDefaultsOnly, Category = Firing)
-    float LaunchSpeed = 4000; 
-    
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
 public:	
     // Sets default values for this pawn's properties
     ATank();
